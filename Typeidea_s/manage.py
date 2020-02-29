cd05541e2e77df1c2871f7e33a2d9454ab2817f0
JunchuangYang <554002970@qq.com>
2020-02-29 22:41:43 +0800

    增加项目Model

--- Typeidea_s/manage.py
+++ Typeidea_s/manage.py
@@ -4,7 +4,7 @@ import sys
 
 if __name__ == "__main__":
     #os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Typeidea_s.settings")
-    profile = os.environ.get('TYPEIDEA_PROFILE','develop')
+    profile = os.environ.get('Typeidea_s','develop')
     os.environ.setdefault("DJANGO_SETTINGS_MODULE","Typeidea_s.settings.%s" % profile)
     try:
         from django.core.management import execute_from_command_line
