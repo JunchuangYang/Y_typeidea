dd986ebda6f60bda9426e99cebe118b16ba5967e
JunchuangYang <554002970@qq.com>
2020-03-06 17:16:25 +0800

    add bootstarp

--- Typeidea_s/Typeidea_s/settings/base.py
+++ Typeidea_s/Typeidea_s/settings/base.py
@@ -57,10 +57,12 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'Typeidea_s.urls'
 
+THEME = 'default'
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'themes', THEME, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -124,3 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'themes', THEME, "static") ,
+]
