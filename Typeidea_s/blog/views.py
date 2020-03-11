6fe7657a4d8f23fb9c1eb71d31376f008c454bf8
JunchuangYang <554002970@qq.com>
2020-03-11 21:59:44 +0800

    完善blog:comment+markdown

--- Typeidea_s/blog/views.py
+++ Typeidea_s/blog/views.py
@@ -1,10 +1,13 @@
 #coding=utf-8
 '''编写 class-based view 代码'''
+from django.db.models import Q
 from django.views.generic import ListView, DetailView
 from django.shortcuts import get_object_or_404
 
 from .models import Post , Tag ,Category
 from config.models import SideBar
+from comment.forms import CommentForm
+from comment.models import Comment
 
 '''通用数据：分类导航、侧边栏、底部导航'''
 class CommonViewMixin:
@@ -85,9 +88,39 @@ class PostDetailView(CommonViewMixin, DetailView):
     context_object_name = 'post'
     pk_url_kwarg = 'post_id'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update({
+            'comment_form':CommentForm,
+            'comment_list':Comment.get_by_target(self.request.path),
+        })
+        return context
 
+'''关键词搜索页'''
+class SearchView(IndexView):
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data()
+        context.update({
+            'keyword':self.request.GET.get('keyword', '')
+        })
+        return context
+    '''
+    覆写了父类的 get_queryset 方法。该方法默认获取指定模型的全部列表数据。
+    为了获取指定分类下的文章列表数据，我们覆写该方法，改变它的默认行为。
+    '''
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        keyword = self.request.GET.get('keyword')
+        if not keyword:
+            return queryset
+        # 因此我们可以判断，contains是精确查询，icontains是忽略大小写的模糊查询
+        return queryset.filter(Q(title__icontains=keyword) | Q(desc__icontains=keyword))
 
-
+class AuthorView(IndexView):
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        author_id = self.kwargs.get('owner_id')
+        return queryset.filter(owner_id=author_id)
 
 
 
